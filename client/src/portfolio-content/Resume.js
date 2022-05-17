156063dd678392f13eb0cf5f4a0d20fe132ccc7a
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-05-17 15:06:55 -0400

    fixing resume

--- client/src/portfolio-content/Resume.js
+++ client/src/portfolio-content/Resume.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
+import resume from "./Resume.pdf";
 import { Grid, Paper } from "@mui/material";
 
+import "./Resume.css";
 const Resume = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
   return (
     <Grid
       container
+      xs={12}
       spacing={0}
       direction="column"
       alignItems="center"
       justifyContent="center"
-      style={{ minHeight: "100vh" }}
+      sx={{ p: 7, minHeight: "100vh" }}
     >
-      <Grid item xs={3}>
-        <Paper variant="outlined">
-          <a>
-            <img src="" />
-          </a>
-        </Paper>
+      <Grid item xs={12}>
+        <Paper variant="outlined"></Paper>
       </Grid>
     </Grid>
   );
